ad147070f1a695e74f797b14eb962608264e601b
Dinara83 <108099459+Dinara83@users.noreply.github.com>
2022-12-17 23:32:18 +0200

    gellary btn

--- src/index.js
+++ src/index.js
@@ -18,43 +18,49 @@ refs.searchForm.addEventListener('submit', searchForm);
 loadMoreBtn.refs.button.addEventListener('click', fetchCardBtn);
 // const simpleLightBox = new SimpleLightbox('.gallery a').refresh();
 
-// const gallery = new SimpleLightbox('.gallery a', {
-//   captionDelay: 250,
-//   overlayOpacity: 0.8,
-//   closeText: 'x',
-//   scrollZoom: false,
-// });
-// gallery.refresh();
+const simpleLightBox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  overlayOpacity: 0.8,
+  closeText: 'x',
+  scrollZoom: false,
+});
+
 async function searchForm(element) {
-  try {
-    element.preventDefault();
+  element.preventDefault();
 
-    fetchApiService.query =
-      element.currentTarget.elements.searchQuery.value.trim();
-
-    if (fetchApiService.query !== true) {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    }
-
-    loadMoreBtn.show();
-    fetchApiService.resetPage();
-    clearGalleryCard();
-
-    fetchCardBtn();
-  } catch (error) {
-    console.log(error);
-  }
+  fetchApiService.query =
+    element.currentTarget.elements.searchQuery.value.trim();
+  fetchApiService.resetPage();
+  clearGalleryCard();
+  loadMoreBtn.disable();
+  await fetchCardBtn();
 }
 
-function fetchCardBtn() {
+async function fetchCardBtn() {
   loadMoreBtn.disable();
-  fetchApiService.fetchCard().then(({ hits }) => {
-    items = hits;
+  fetchApiService.incrementPage();
+  await fetchApiService
+    .fetchCard()
+    .then(({ hits }) => {
+      items = hits;
+      renderGalleryCard();
+      simpleLightBox.refresh();
+    })
+    .catch(error => {
+      Notiflix.Notify.failure(
+        `Sorry, there are no images matching your search query. Please try again.`
+      );
+    });
+
+  if (!data.totalHits) {
+    Notiflix.Notify.failure(
+      `Sorry, there are no images matching your search query. Please try again.`
+    );
+  } else {
     renderGalleryCard();
-    loadMoreBtn.enable();
-  });
+    Notiflix.Notify.failure(`Hooray! We found ${data.totalHits} images.`);
+  }
+  loadMoreBtn.enable();
 }
 
 const renderGalleryCard = () => {
